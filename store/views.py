a454b4358e7abd03fb90edc27528a95bd9f8c481
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-06-30 14:46:10 +0200

    Replace get post with inherited functions from ListCreateAPIView

--- store/views.py
+++ store/views.py
@@ -10,18 +10,28 @@ from django.db.models.aggregates import Count
 from rest_framework.views import APIView
 
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(
-            queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+class ProductList(ListCreateAPIView):
+
+    def get_queryset(self):
+        return Product.objects.select_related('collection').all()
     
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def get_serializer_class(self):
+        return ProductSerializer
+    
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    # def get(self, request):
+    #     queryset = Product.objects.select_related('collection').all()
+    #     serializer = ProductSerializer(
+    #         queryset, many=True, context={'request': request})
+    #     return Response(serializer.data)
+    
+    # def post(self, request):
+    #     serializer = ProductSerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ProductDetail(APIView):
