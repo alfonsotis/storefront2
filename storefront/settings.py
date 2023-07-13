7de5d55eb0078331bfbc8b186f7534a492bd4eb8
alfonsotis <58032826+alfonsotis@users.noreply.github.com>
2023-07-13 21:08:31 +0200

    remove DEFAULT_PERMISSION_CLASSES from settings

--- storefront/settings.py
+++ storefront/settings.py
@@ -151,10 +151,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     
-    'DEFAULT_PERMISSION_CLASSES': [
+    # 'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.AllowAny'
-        'rest_framework.permissions.IsAuthenticated'
-    ]
+    #     'rest_framework.permissions.IsAuthenticated'
+    # ]
 }
 
 
